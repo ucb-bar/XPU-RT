c5e508fc3848e6b560d554754bcfe090a12554c4
ailsa-sun <jjshenw@gmail.com>
2026-08-21 14:25:58 -0700

    delete xpu

--- xpu-rt/pytorch_workload/onnx_compilation_script/build_onnx.sh
+++
@@ -1,9 +0,0 @@
-iree-import-onnx -o --opset-version=17  ../../mlir_pipelining_script/workload_mlir/fastdepth.mlir ../fastdepth/MobileNetSkipAdd.onnx
-
-python3 ../dronet/dronet.py
-iree-import-onnx -o --opset-version=17   ../../mlir_pipelining_script/workload_mlir/dronet.mlir ../dronet/dronet.onnx
-
-python3 ../mlp/simple_mlp.py
-iree-import-onnx -o --opset-version=17   ../../mlir_pipelining_script/workload_mlir/mlp.mlir ../mlp/simple_mlp.onnx
-
-
